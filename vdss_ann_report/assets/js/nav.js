0e300a5582404f871a62320e6172a754b198940d
Jacques (VDSS) <jacques.walker@dss.virginia.gov>
2016-07-12 17:00:56 -0400

    added js code for pageID display

--- vdss_ann_report/assets/js/nav.js
+++ vdss_ann_report/assets/js/nav.js
@@ -1,3 +1,4 @@
+/* Javascript for hover function */
 $('nav').hover(
 	function(){
 		$('.title').fadeIn();
@@ -6,3 +7,20 @@ $('nav').hover(
 		$('.title').fadeOut();
 	}
 );
+/*Javascript for div select and display */
+$(document).ready(function($){
+	function getUrlVars() {
+		var vars = {};
+		var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+		vars[key] = value;
+	});
+	return vars;
+		}
+	var pageID = getUrlVars()["pageID"];
+	var i = 0;
+	$(".tableauPlaceholder").each(function(){
+		i++;
+		$(this).addClass('divID_'+i);
+	});
+		$('.divID_'+pageID).addClass('show');
+});
