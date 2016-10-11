c9ba65eb47dea163cfb5aeabe3d07391842e955e
Jacques Walker <jawalker@vt.edu>
2016-10-11 16:38:35 -0400

    added social media icons, footer nav, move scroll effect to external js file, placeholder links added

---
+++ natl_adoption/assets/js/scroll_effect.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+		var i = 0;
+		$(".hashtag").each(function(){
+		i++;
+		$(this).addClass('div_'+i);
+		});
+	   $(window).bind('scroll', function() {
+	   var navHeight = $( window ).height() - 78;
+			 if ($(window).scrollTop() > navHeight) {
+				 $('nav').addClass('navbar-fixed-top');
+			 }
+			 else {
+				 $('nav').removeClass('navbar-fixed-top');				 
+			 }
+		/* Code for fixed hashtags */
+		/*var navHeight2 = $(window).height() - 150;
+			 if ($(window).scrollTop() > navHeight2) {
+				 $('.div_1').addClass('fixed_top');
+			 }
+			 else {
+				 $('.div_1').removeClass('fixed_top');				 
+			 }			
+		var navHeight3 = $(window).height() + 1050;
+			 if ($(window).scrollTop() > navHeight3) {
+				 $('.div_2').addClass('fixed_top2');
+			 }
+			 else {
+				 $('.div_2').removeClass('fixed_top2');				 
+			 }	
+		var navHeight4 = $(window).height() + 2270;
+			 if ($(window).scrollTop() > navHeight4) {
+				 $('.div_3').addClass('fixed_top3');
+			 }
+			 else {
+				 $('.div_3').removeClass('fixed_top3');				 
+			 }	
+		var navHeight5 = $(window).height() + 3500;
+			 if ($(window).scrollTop() > navHeight5) {
+				 $('.div_4').addClass('fixed_top4');
+			 }
+			 else {
+				 $('.div_4').removeClass('fixed_top4');				 
+			 }*/		 
+		});
+		console.log("Window position is "+ $(window).height());
+	});
