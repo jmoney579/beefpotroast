967b2d0a95f4d3aa35aad77d043ea974890afbf3
Jacques Walker <jawalker@vt.edu>
2017-04-26 15:33:25 -0400

    added cookie.js and editied form submission to read cookie to determine display

--- FosterMyFuture/js/feedback.js
+++ FosterMyFuture/js/feedback.js
@@ -18,8 +18,32 @@ $(function() {
 });
 
 /*---JS for submission message ----*/
-$("#feedback-form form").submit(function() {
-		$("#feedback-form form").hide();
-		$(".submit_complete").fadeIn("slow");
-		$("feedback-form").hide().delay(1200);
-});
+
+/* $(document).ready(function(){   
+  if ($.cookie('fostercare_contact') == null ){
+   var ref = document.referrer.toLowerCase();  
+   // set cookie  
+   var cookURL =  $.cookie('fostercare_contact', ref, { expires: 1 }); 
+  } 
+ });  */
+
+	
+	 $(document).ready(function(){            
+
+         if (Cookies.get('fostercarecontact')) { 		
+			$("#feedback-form form").hide();
+			$(".submit_complete").fadeIn("slow");
+			$("feedback-form").hide().delay(2400);
+			console.log(Cookies.get('fostercarecontact'));
+         }else{
+			 $("#feedback-form form").submit(function() {
+                Cookies.set('fostercarecontact', 'submitted', { expires: 7});
+				$("#feedback-form form").hide();
+				$(".submit_complete").fadeIn("slow");
+				$("feedback-form").hide().delay(2400);
+				console.log(Cookies.get('fostercarecontact'));
+			}); 
+		 }
+	 });
+
+	
