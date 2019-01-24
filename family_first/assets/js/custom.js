9bf885e04ad96fd2940085303ce0ab63f6163b0d
Jacques Walker <jawalker@vt.edu>
2019-01-24 16:52:06 -0500

    added js for search and translate bars, cleaned up some mobile stuff

--- family_first/assets/js/custom.js
+++ family_first/assets/js/custom.js
@@ -1,7 +1,20 @@
 			$(".search-button").click(function(){
+				if ($(".translate-bar").is(":visible")){
+					$(".translate-bar").hide();
+				}
 				if ($(".search-bar").is(":hidden")){
 						$(".search-bar").fadeIn();
 					}else{
 						$(".search-bar").fadeOut();
+							}
+				})
+			$(".translate-button").click(function(){
+				if ($(".search-bar").is(":visible")){
+					$(".search-bar").hide();
+				}
+				if ($(".translate-bar").is(":hidden")){
+						$(".translate-bar").fadeIn();
+					}else{
+						$(".translate-bar").fadeOut();
 							}
 				})
