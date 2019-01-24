4a9410a41fd6abf20c30743272ea55b7162b1a45
Jacques Walker <jawalker@vt.edu>
2019-01-24 14:35:57 -0500

    added header and call out image, cleaned up nav

---
+++ family_first/assets/js/custom.js
@@ -0,0 +1,7 @@
+			$(".search-button").click(function(){
+				if ($(".search-bar").is(":hidden")){
+						$(".search-bar").fadeIn();
+					}else{
+						$(".search-bar").fadeOut();
+							}
+				})
